77530c7039dfdb4ed5a001ca28876e4cfc50b17c
kedar16 <kedar16@gwmail.gwu.edu>
2019-06-16 14:27:51 -0400

    Initial commit

---
+++ app/src/main/java/kedar/com/movies/ui/MainActivity.kt
@@ -0,0 +1,13 @@
+package kedar.com.movies.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kedar.com.movies.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
